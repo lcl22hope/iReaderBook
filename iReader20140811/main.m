1f4e9049d71946e1700440d6f0ab885cf69fa1d3
lcl <lcl@lcl-OSXs-MacBook-Pro.local>
2014-08-11 15:19:39 +0800

    Initial Commit

---
+++ iReader20140811/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iReader20140811
+//
+//  Created by lcl on 11/8/14.
+//  Copyright (c) 2014 lcl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
